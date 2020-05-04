f48b13dedfb98b26ab0975f5492d4be247e10d8e
bizhengyong <bizhengyong_xy@163.com>
2020-05-04 15:22:04 +0800

    5-7-扣库存（Feign）

--- src/main/java/com/imooc/order/client/ProductClient.java
+++ src/main/java/com/imooc/order/client/ProductClient.java
@@ -1,6 +1,7 @@
 package com.imooc.order.client;
 
 import com.imooc.order.dataobject.ProductInfo;
+import com.imooc.order.dto.CartDTO;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,4 +21,7 @@ public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
     List<ProductInfo> listForOrder(@RequestBody List<String> productIdList);
+
+    @PostMapping("/product/decreaseStock")
+    public void decreaseStock (@RequestBody List<CartDTO> cartDTOList);
 }
