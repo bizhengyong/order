b5f16a1b9b0f12ec29d7bfc040b753aa73bc4710
bizhengyong <bizhengyong_xy@163.com>
2020-05-05 19:53:18 +0800

    5-9-项目改造成多模块

--- server/src/main/java/com/imooc/order/controller/ClientController.java
+++ server/src/main/java/com/imooc/order/controller/ClientController.java
@@ -1,8 +1,8 @@
 package com.imooc.order.controller;
 
-import com.imooc.order.client.ProductClient;
-import com.imooc.order.dataobject.ProductInfo;
-import com.imooc.order.dto.CartDTO;
+import com.imooc.product.client.ProductClient;
+import com.imooc.product.common.DecreaseStockInput;
+import com.imooc.product.common.ProductInfoOutPut;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,24 +22,16 @@ public class ClientController {
     @Autowired
     private ProductClient productClient;
 
-    @GetMapping("/getProductMsg")
-    public String getProductMsg(){
-
-        String response = productClient.productMsg();
-        log.info("response={}",response);
-        return response;
-    }
-
     @GetMapping("/getProductList")
     public String getProductList(){
-        List<ProductInfo> productInfoList = productClient.listForOrder(Arrays.asList("164103465734242707"));
+        List<ProductInfoOutPut> productInfoList = productClient.listForOrder(Arrays.asList("164103465734242707"));
         log.info("response={}",productInfoList);
         return "ok";
     }
 
     @GetMapping("/productDecreaseStock")
     public String productDecreaseStock(){
-        productClient.decreaseStock(Arrays.asList(new CartDTO("164103465734242707",3)));
+        productClient.decreaseStock(Arrays.asList(new DecreaseStockInput("164103465734242707",3)));
         return "ok";
     }
 }
