43c6fe7a19d1e03cc4e6ad3d3c605a30493df357
bizhengyong <bizhengyong_xy@163.com>
2020-05-14 22:10:19 +0800

    7-5-SpringCloud Stream的使用（下）

---
+++ server/src/main/java/com/imooc/order/message/StreamClient.java
@@ -0,0 +1,31 @@
+package com.imooc.order.message;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+
+import java.util.stream.Stream;
+
+/**
+ * @author: bizy
+ * @date: 2020/05/14 21:16
+ */
+public interface StreamClient {
+
+    String INPUT = "myMessage";
+
+    String INPUT2 = "myMessage2";
+
+    @Input(StreamClient.INPUT)
+    SubscribableChannel input();
+
+    @Output(StreamClient.INPUT)
+    MessageChannel output();
+
+    @Input(StreamClient.INPUT2)
+    SubscribableChannel input2();
+
+    @Output(StreamClient.INPUT2)
+    MessageChannel output2();
+}
