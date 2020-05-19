584c15b59e61991bf8f56a44f541aec463b8efe4
bizhengyong <bizhengyong_xy@163.com>
2020-05-19 21:02:11 +0800

    7-8-商品和订单服务中使用MQ(下)

--- server/src/main/java/com/imooc/order/message/ProductInfoReceiver.java
+++ server/src/main/java/com/imooc/order/message/ProductInfoReceiver.java
@@ -1,5 +1,6 @@
 package com.imooc.order.message;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.imooc.order.utils.JsonUtil;
 import com.imooc.product.common.ProductInfoOutPut;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,8 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author: bizy
@@ -25,11 +28,14 @@ public class ProductInfoReceiver {
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message) {
         //message => ProductInfoOutPut
-        ProductInfoOutPut productInfoOutPut = (ProductInfoOutPut) JsonUtil.fromJson(message, ProductInfoOutPut.class);
-        log.info("从队列【{}】接收到消息:{}","productInfo",productInfoOutPut);
+        List<ProductInfoOutPut> productInfoOutPutList = (List<ProductInfoOutPut>) JsonUtil.fromJson(message,
+                new TypeReference<List<ProductInfoOutPut>>(){});
+        log.info("从队列【{}】接收到消息:{}","productInfo",productInfoOutPutList);
 
         //存储到redis中
-        stringRedisTemplate.opsForValue().set(String.format(PRODUCT_STOCK_TEMPLATE,productInfoOutPut.getProductIcon()),
-                String.valueOf(productInfoOutPut.getProductStock()));
+        for (ProductInfoOutPut productInfoOutPut : productInfoOutPutList) {
+            stringRedisTemplate.opsForValue().set(String.format(PRODUCT_STOCK_TEMPLATE,productInfoOutPut.getProductIcon()),
+                    String.valueOf(productInfoOutPut.getProductStock()));
+        }
     }
 }
--- server/src/main/java/com/imooc/order/utils/JsonUtil.java
+++ server/src/main/java/com/imooc/order/utils/JsonUtil.java
@@ -1,6 +1,7 @@
 package com.imooc.order.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -14,12 +15,12 @@ public class JsonUtil {
     private static ObjectMapper objectMapper = new ObjectMapper();
 
     /**
-    *@description: 转换为json字符串
-    *@param: object
-    *@return: 
-    *@author: bizy
-    *@date: 2020/5/14 22:49
-    */
+     *@description: 转换为json字符串
+     *@param: object
+     *@return:
+     *@author: bizy
+     *@date: 2020/5/14 22:49
+     */
     public static String toJson(Object object) {
         try {
             return objectMapper.writeValueAsString(object);
@@ -30,15 +31,31 @@ public class JsonUtil {
     }
 
     /**
-    *@description: js转对象
-    *@param: 
-    *@return: 
-    *@author: bizy
-    *@date: 2020/5/19 19:09
-    */
+     *@description: js转对象
+     *@param:
+     *@return:
+     *@author: bizy
+     *@date: 2020/5/19 19:09
+     */
     public static Object fromJson(String string,Class classType) {
         try {
             return objectMapper.readValue(string,classType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *@description: js转对象
+     *@param:
+     *@return:
+     *@author: bizy
+     *@date: 2020/5/19 20:55
+     */
+    public static Object fromJson(String string, TypeReference typeReference) {
+        try {
+            return objectMapper.readValue(string,typeReference);
         } catch (IOException e) {
             e.printStackTrace();
         }
