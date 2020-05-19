4a5923da33565d1fc8b3b2e710dc2fc750e907b6
bizhengyong <bizhengyong_xy@163.com>
2020-05-19 20:12:24 +0800

    7-7-商品和订单服务中使用MQ(中)

--- server/src/main/java/com/imooc/order/message/ProductInfoReceiver.java
+++ server/src/main/java/com/imooc/order/message/ProductInfoReceiver.java
@@ -1,8 +1,12 @@
 package com.imooc.order.message;
 
+import com.imooc.order.utils.JsonUtil;
 import com.imooc.product.common.ProductInfoOutPut;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,10 +14,22 @@ import org.springframework.stereotype.Component;
  * @date: 2020/05/14 23:06
  */
 @Component
+@Slf4j
 public class ProductInfoReceiver {
+
+    public static final String PRODUCT_STOCK_TEMPLATE = "product_stock_%s";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message) {
         //message => ProductInfoOutPut
+        ProductInfoOutPut productInfoOutPut = (ProductInfoOutPut) JsonUtil.fromJson(message, ProductInfoOutPut.class);
+        log.info("从队列【{}】接收到消息:{}","productInfo",productInfoOutPut);
+
+        //存储到redis中
+        stringRedisTemplate.opsForValue().set(String.format(PRODUCT_STOCK_TEMPLATE,productInfoOutPut.getProductIcon()),
+                String.valueOf(productInfoOutPut.getProductStock()));
     }
 }
---
+++ server/src/main/java/com/imooc/order/utils/JsonUtil.java
@@ -0,0 +1,47 @@
+package com.imooc.order.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * @author: bizy
+ * @date: 2020/05/14 22:47
+ */
+public class JsonUtil {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+    *@description: 转换为json字符串
+    *@param: object
+    *@return: 
+    *@author: bizy
+    *@date: 2020/5/14 22:49
+    */
+    public static String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+    *@description: js转对象
+    *@param: 
+    *@return: 
+    *@author: bizy
+    *@date: 2020/5/19 19:09
+    */
+    public static Object fromJson(String string,Class classType) {
+        try {
+            return objectMapper.readValue(string,classType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
