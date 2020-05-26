182dadf3d4a1c8d30e7e864fb3c2b6d020630d59
bizhengyong <bizhengyong_xy@163.com>
2020-05-26 20:52:37 +0800

    9-6-完结订单接口开发

--- server/src/main/java/com/imooc/order/service/OrderService.java
+++ server/src/main/java/com/imooc/order/service/OrderService.java
@@ -15,4 +15,14 @@ public interface OrderService {
      * @date: 2020/5/3 12:06
      */
     public OrderDTO create(OrderDTO orderDTO);
+    
+    /**
+    *@description: 完结订单（只能卖家操作）
+    *@param: 
+    *@return:
+    *@author: bizy
+    *@date: 2020/5/26 20:23
+    */
+    OrderDTO finish(String orderId);
+    
 }
